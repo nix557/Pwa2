1e213224e24bd22dc4c965ec8c5b556b2401ac41
nix557 <nixonop128@gmail.com>
2025-09-29 11:00:43 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,6 @@ const CACHE_NAME = 'gemini-chat-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/style.css',
-  '/script.js',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
